ebda07fe9ff3bfc662d59797b5e210975431a220
Sushobhit jain <sushobhit.builtbyblank@gmail.com>
2017-09-25 18:33:37 +0530

    add constant file

---
+++ Assignment/Utility/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Assignment
+//
+//  Created by Sushobhit Jain on 24/09/17.
+//  Copyright © 2017 Sushobhit Jain. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+// MARK: API Keys
+struct SD2Lab {
+    static let ApiScheme = "http"
+    static let ApiHost = "sd2-hiring.herokuapp.com"
+//    static let ApiPort = 8080
+    static let ApiPath = "/api/"
+}
+
+// MARK: Methods
+struct Methods {
+    static let users = "users"
+}
